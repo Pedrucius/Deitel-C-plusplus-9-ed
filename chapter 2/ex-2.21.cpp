526cc1dbb4f6cfa2a448409875326aed41016bc9
Pedrucius <fabiopedruci@gmail.com>
2017-04-17 20:51:12 -0300

    Chapter 2 - Exercise 2.21

---
+++ chapter 2/ex-2.21.cpp
@@ -0,0 +1,27 @@
+/*
+Answer by Fabio Pedruci
+
+Exercise 2.21
+Write a program that prints a box, an oval, an arrow and
+a diamond.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "*********        ***          *             *    " << endl;
+    cout << "*       *      *     *       ***           * *   " << endl;
+    cout << "*       *     *       *     *****         *   *  " << endl;
+    cout << "*       *     *       *       *          *     * " << endl;
+    cout << "*       *     *       *       *         *       *" << endl;
+    cout << "*       *     *       *       *          *     * " << endl;
+    cout << "*       *     *       *       *           *   *  " << endl;
+    cout << "*       *      *     *        *            * *   " << endl;
+    cout << "*********        ***          *             *    " << endl;
+
+    return 0;
+}
+
