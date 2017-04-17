44c80d0627aa757137a78eba015c65969ad65cab
Pedrucius <fabiopedruci@gmail.com>
2017-04-17 12:18:56 -0300

    Create 2.09.cpp

---
+++ chapter 2/2.09.cpp
@@ -0,0 +1,26 @@
+/*
+Answer by Fabio Pedruci
+
+Exercise 2.9
+Write a single C++ statement or line that accomplishes each of the following:
+a) Print the message "Enter two numbers".
+b) Assign the product of variables b and c to variable a.
+c) State that a program performs a payroll calculation (i.e., use text that helps to document
+a program).
+d) Input three integer values from the keyboard into integer variables a, b and c.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b, c; //not a good practice, but for this exercise, it's ok.
+    cout << "Enter Two Numbers:" << endl;
+    cin >> b >> c;
+    a = b * c;
+    cout << "This program performs a a payroll calculation.\n";
+    cin >> a >> b >> c;
+    return 0;
+}
