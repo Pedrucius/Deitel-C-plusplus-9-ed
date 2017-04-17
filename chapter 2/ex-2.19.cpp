34ed5addaf767618e4b2efc5c73623ae8cc1c0b3
Pedrucius <fabiopedruci@gmail.com>
2017-04-17 19:24:26 -0300

    Chapter 2 - Exercise 2.19

---
+++ chapter 2/ex-2.19.cpp
@@ -0,0 +1,51 @@
+/*
+Answer by Fabio Pedruci
+
+Exercise 2.19
+Write a program that inputs three integers from the keyboard
+and prints the sum, average, product, smallest and largest of these numbers.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num1 = 0;
+    int num2 = 0;
+    int num3 = 0;
+    int largest = 0;
+    int smallest = 0;
+    cout << "Please enter three numbers:" << endl;
+    cin >> num1;
+    largest = num1;
+    smallest = num1;
+    cin >> num2;
+    if (largest < num2){
+        largest = num2;
+    }
+    else if (smallest > num2){
+        smallest = num2;
+    }
+    cin >> num3;
+    if (largest < num3){
+        largest = num3;
+    }
+    else if (smallest > num3){
+        smallest = num3;
+    }
+
+    cout << "The sum is: " << num1 + num2 + num3 << endl;
+    cout << "The average is: " << (num1 + num2 + num3)/3 << endl;
+    cout << "The product is: " << num1 * num2 * num3 << endl;
+    cout << "The smallest is: " << smallest << endl;
+    cout << "The largest is: " << largest << endl;
+
+    return 0;
+}
+
+
+
+
+
